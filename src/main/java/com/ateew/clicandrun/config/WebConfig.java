1e7b322ac345c2efb03ef2f55ea7dbdc16c3c776
atteewf <ateeew@gmail.com>
2026-08-13 22:46:27 +0200

    ajout de vercel au Webconfig

--- src/main/java/com/ateew/clicandrun/config/WebConfig.java
+++ src/main/java/com/ateew/clicandrun/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173","https://ton-url.vercel.app")
+            .allowedOrigins("http://localhost:5173","https://clicandrun-blue.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
 }
